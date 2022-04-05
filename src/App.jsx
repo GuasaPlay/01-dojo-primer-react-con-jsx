56ee9d168712cb3cda92b08f79681a05a942f19f
Oscar Calle <gameplaysoscar@gmail.com>
2022-04-04 22:46:04 -0500

    Task completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { List } from './components/List';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello Dojo</h1>
+			<h2>Things I need</h2>
+			<List />
+		</div>
+	);
+}
+
+export default App;
